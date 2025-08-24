4cc8261ecfad4ddbfd2e8bd3d7ebde6cfdc5ad47
MSoloz <atefmiladi8@gmail.com>
2025-08-24 13:55:29 +0100

    first commit

---
+++ src/main/java/com/melisima/eventek/EventekApplication.java
@@ -0,0 +1,13 @@
+package com.melisima.eventek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventekApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/melisima/eventek/EventekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melisima.eventek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
